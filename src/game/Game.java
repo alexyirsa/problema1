11906b6e53605209059a798edd83b58dedf052b4
alexyirsa <v.yirsa@uniandes.edu.co>
2023-11-27 21:35:09 -0500

    Refactor de programa para eliminar código duplicado

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,144 +1,103 @@
 package game;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Random;
 
 public class Game {
 
-	public void play() {
-		int player = 0;
-		String[][] board = {{"⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬜"},
-				{"⬜", "⬜", "⬜", "⬛", "⬛", "⬛", "⬜", "⬜"},
-				{"⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬛", "⬜"},
-				{"⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬛", "⬜"},
-				{"⬜", "⬜", "⬜", "⬜", "⬜", "⬛", "⬜", "⬜"},
-				{"⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬛", "⬜"},
-				{"⬜", "⬜", "⬜", "⬜", "⬜", "⬜", "⬛", "⬜"},
-				{"⬜", "⬜", "⬜", "⬛", "⬛", "⬛", "⬜", "⬜"}};
+    private static final String EMPTY = "⬜";
+    private static final String OBSTACLE = "⬛";
+    private static final String CAR = "🚙";
+    private static final String PLAYER_CAR = "🚗";
 
-		for(String[] row : board) {
-			for(String elem : row)
-				System.out.print(" " + elem + " ");
-			System.out.println("");
-		}
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			Thread.currentThread().interrupt();
-		}
+    private String[][] board;
+    private int player;
 
-		for(int i=0; i<board[0].length; i++) board[0][i] = "⬜";
-		for(int i=0; i<board[1].length; i++) board[0][i] = "⬜";
-		for(int i=0; i<board[2].length; i++) board[0][i] = "⬜";
-		for(int i=0; i<board[3].length; i++) board[0][i] = "⬜";
-		for(int i=0; i<board[4].length; i++) board[0][i] = "⬜";
-		for(int i=0; i<board[5].length; i++) board[0][i] = "⬜";
-		for(int i=0; i<board[6].length; i++) board[0][i] = "⬜";
-		for(int i=0; i<board[7].length; i++) board[0][i] = "⬜";
-		board[1][3] = "⬛"; board[1][4] = "⬛"; board[1][5] = "⬛";
-		board[2][2] = "⬛"; board[2][5] = "⬛"; 
-		board[3][6] = "⬛"; 
-		board[4][5] = "⬛"; 
-		board[5][4] = "⬛"; 
-		board[6][3] = "⬛"; 
-		board[7][2] = "⬛"; board[7][3] = "⬛"; board[7][4] = "⬛"; board[7][5] = "⬛"; board[7][6] = "⬛";
+    public Game() {
+        initializeBoard();
+        player = 0;
+    }
 
-		for(String[] row : board) {
-			for(String r : row)
-				System.out.print(" " + r + " ");
-			System.out.println("");
-		}
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			Thread.currentThread().interrupt();
-		}
+    private void initializeBoard() {
+        board = new String[][]{
+                {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                {EMPTY, EMPTY, EMPTY, OBSTACLE, OBSTACLE, OBSTACLE, EMPTY, EMPTY},
+                {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, OBSTACLE, EMPTY},
+                {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, OBSTACLE, EMPTY},
+                {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, OBSTACLE, EMPTY, EMPTY},
+                {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, OBSTACLE, EMPTY},
+                {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, OBSTACLE, EMPTY},
+                {EMPTY, EMPTY, EMPTY, OBSTACLE, OBSTACLE, OBSTACLE, EMPTY, EMPTY}
+        };
+    }
 
-		for(int i=0; i<board[0].length; i++) board[0][i] = "⬜";
-		for(int i=0; i<board[1].length; i++) board[0][i] = "⬜";
-		for(int i=0; i<board[2].length; i++) board[0][i] = "⬜";
-		for(int i=0; i<board[3].length; i++) board[0][i] = "⬜";
-		for(int i=0; i<board[4].length; i++) board[0][i] = "⬜";
-		for(int i=0; i<board[5].length; i++) board[0][i] = "⬜";
-		for(int i=0; i<board[6].length; i++) board[0][i] = "⬜";
-		for(int i=0; i<board[7].length; i++) board[0][i] = "⬜";
-		board[1][4] = "⬛"; 
-		board[2][3] = "⬛"; board[2][3] = "⬛"; 
-		board[3][4] = "⬛"; 
-		board[4][4] = "⬛"; 
-		board[5][4] = "⬛"; 
-		board[6][4] = "⬛"; 
-		board[7][2] = "⬛"; board[7][3] = "⬛"; board[7][4] = "⬛"; board[7][5] = "⬛"; 
+    public void play() {
+        boolean playing = true;
+        while (playing) {
+            printBoard();
+            try {
+                moveCars();
+                handlePlayerInput();
+                playing = checkCollision();
+                Thread.sleep(1000);
+            } catch (InterruptedException | IOException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        System.out.println("Perdiste!");
+    }
 
-		for(String[] row : board) {
-			for(String r : row)
-				System.out.print(" " + r + " ");
-			System.out.println("");
-		}
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			Thread.currentThread().interrupt();
-		}
+    private void printBoard() {
+        for (String[] row : board) {
+            for (String elem : row)
+                System.out.print(" " + elem + " ");
+            System.out.println("");
+        }
+    }
 
-		for(int i=0; i<board[0].length; i++) board[0][i] = "⬜";
-		for(int i=0; i<board[1].length; i++) board[0][i] = "⬜";
-		for(int i=0; i<board[2].length; i++) board[0][i] = "⬜";
-		for(int i=0; i<board[3].length; i++) board[0][i] = "⬜";
-		for(int i=0; i<board[4].length; i++) board[0][i] = "⬜";
-		for(int i=0; i<board[5].length; i++) board[0][i] = "⬜";
-		for(int i=0; i<board[6].length; i++) board[0][i] = "⬜";
-		for(int i=0; i<board[7].length; i++) board[0][i] = "⬜";
+    private void moveCars() {
+        for (int i = board.length - 1; i > 0; i--) {
+            board[i] = board[i - 1].clone();
+        }
+        for (int i = 0; i < board[0].length; i++) {
+            board[0][i] = EMPTY;
+        }
+        generateRandomCar();
+    }
 
-		boolean playing = true;
-		while(playing) {
-			try {
-				Random r = new Random();
-				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-				int newCar = r.nextInt(8);
-				board[0][newCar] = "🚙";
-				board[7][player] = "🚗";
-				
-				for(String[] row : board) {
-					for(String a : row)
-						System.out.print(" " + a + " ");
-					System.out.println("");
-				}
+    private void generateRandomCar() {
+        Random random = new Random();
+        int newCar = random.nextInt(8);
+        board[0][newCar] = CAR;
+    }
 
-				//input
-				String key = br.readLine();
+    private void handlePlayerInput() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String key = br.readLine();
 
-				if(key.equals("q")) {
-					playing = false;
-					break;
-				} else if(key.equals("a") && player > 0) {
-					board[7][player] = "⬜";
-					player -= 1;
-				} else if(key.equals("d") && player < 7) {
-					board[7][player] = "⬜";
-					player += 1;
-				}
-				if(board[7][player].equals("🚙") || board[6][player].equals("🚙")) {
-					playing = false;
-					System.out.println("Perdiste!");
+        if ("q".equals(key)) {
+            System.exit(0);
+        } else if ("a".equals(key) && player > 0) {
+            movePlayer(-1);
+        } else if ("d".equals(key) && player < 7) {
+            movePlayer(1);
+        }
+        board[7][player] = PLAYER_CAR;
+    }
 
-					//move cars down
-					for(int i=0; i<7; i++) 
-						board[7-i] = board[6-i];
+    private void movePlayer(int direction) {
+        board[7][player] = EMPTY;
+        player += direction;
+    }
 
-					for(int i=0; i<board[0].length; i++) board[0][i] = "⬜";
-				}
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
+    private boolean checkCollision() {
+        return board[7][player].equals(CAR) || board[6][player].equals(CAR);
+    }
 
-
-	public static void main(String[] args) {
-		Game game = new Game();
-		game.play();
-	}
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.play();
+    }
 }
